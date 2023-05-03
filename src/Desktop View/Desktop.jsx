326c8049152a8944d8f81181aeb9bbf93954fadb
Dacron <favouradebimpe63@gmail.com>
2023-05-03 06:34:21 +0100

    testing again..

--- src/Desktop View/Desktop.jsx
+++ src/Desktop View/Desktop.jsx
@@ -7,7 +7,7 @@ import background from '/src/assets/home/background-home-desktop.jpg'
 
 const useStyles = makeStyles({
   root: {
-    backgroundImage: 'url("/src/assets/home/background-home-desktop.jpg")',
+    backgroundImage: `url(${background})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundPosition: 'center',

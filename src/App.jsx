3b5b985bc3f6875e4c0adcba6c976bd41cc47468
Dacron <favouradebimpe63@gmail.com>
2023-05-03 05:58:09 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Desktop from './Desktop View/Desktop'
+import Mobile from './Mobile View/Mobile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Desktop />
+     <Mobile />
     </>
   )
 }
--- src/Desktop View/Desktop.jsx
+++ src/Desktop View/Desktop.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import { Box, Paper, Typography, Stack,  createTheme, ThemeProvider } from '@mui/material'
 import './Desktop.css'
+import {makeStyles} from '@mui/styles'
+import background from '/src/assets/home/background-home-desktop.jpg'
+
+
+const useStyles = makeStyles({
+  root: {
+    backgroundImage: 'url("/src/assets/home/background-home-desktop.jpg")',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    // '@media (max-width: 600px)': {
+    //   backgroundImage: 'url("your-image-url-for-small-screens")',
+    //  },
+  },
+});
 
 function Desktop() {
+
+  const classes = useStyles();
+
   return (
-    <div id='desktop-view'>
-      big boooyyy
-    </div>
+    <Box id='desktop-view' className={classes.root} >
+      big boooyyyfddss
+    </Box>
   )
 }
 

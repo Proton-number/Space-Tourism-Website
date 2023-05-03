679f994799ae8bf5181c7eb3879df3aa6f4f4833
Dacron <favouradebimpe63@gmail.com>
2023-05-03 07:53:56 +0100

    trying to adjust

--- src/Desktop View/Desktop.jsx
+++ src/Desktop View/Desktop.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import { Box, Paper, Typography, Stack,  createTheme, ThemeProvider } from '@mui/material'
 import './Desktop.css'
-import {makeStyles} from '@mui/styles'
 import background from '/src/assets/home/background-home-desktop.jpg'
 
 
-const useStyles = makeStyles({
-  root: {
-    backgroundImage: `url(${background})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '100vh',
-     '@media (max-width: 600px)': {
-      backgroundImage: 'url("/src/assets/home/background-home-tablet.jpg")',
-      },
-  },
-});
 
 function Desktop() {
 
-  const classes = useStyles();
 
   return (
-    <Box id='desktop-view' className={classes.root} >
+    <Box 
+    id='desktop-view' 
+    sx={{
+      backgroundImage:`url(${background})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '100vh',
+       '@media (max-width: 600px)': {
+        backgroundImage: 'url("/src/assets/home/background-home-tablet.jpg")',
+        },
+    }}
+     >
       big boooyyyfddss
     </Box>
   )
